facaddf79e5c6d72fca188bc9a5bbe1ef4a53bab
Raynne Nogueira <raynnenogueira123@hotmail.com>
2023-12-02 18:36:11 -0300

    app vue

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,25 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HeaderMain></HeaderMain>
+  <ProductsMain></ProductsMain>
+  <FooterMain></FooterMain>
+  <main>
+    <router-view/>
+  </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  import HeaderMain from './components/HeaderMain.vue';
+  import ProductsMain from './components/ProductsMain.vue';
+  import FooterMain from './components/FooterMain.vue';
+  
+  export default{
+    components: {
+      HeaderMain,
+      ProductsMain,
+      FooterMain
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/views/ProductsView.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+      <h2>Carrinho de Compras</h2>
+      <ProductsView :cartItems="cartItems" />
+      <p>Total: R$ {{ calculateTotal() }}</p>
+    </div>
+  </template>
+  
+  <script>
+  import ProductsView from '@/components/ProductsView.vue';
+  
+  export default {
+    components: {
+      ProductsView,
+    },
+    data() {
+      return {
+        cartItems: [],  
+      };
+    },
+    methods: {
+      calculateTotal() {
+        return 0;  
+      },
+    },
+  };
+  </script>
+  
